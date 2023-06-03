177d17cc37d11a2d540ac6f8a2f7c27f03f677b6
Nathália <nathaliamassis@gmail.com>
2023-06-02 21:46:12 -0300

    test

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -1,115 +1,122 @@
-import { afterAll, beforeAll, beforeEach, describe, expect, test } from 'vitest'
-import { execSync } from 'node:child_process'
-import supertest from 'supertest'
-import { app } from '../src/app'
+import {
+  afterAll,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  test,
+} from "vitest";
+import { execSync } from "node:child_process";
+import supertest from "supertest";
+import { app } from "../src/app";
 
-describe('Transactions', () => {
+describe("Transactions", () => {
   beforeAll(async () => {
-    await app.ready()
-  })
+    await app.ready();
+  });
 
   afterAll(async () => {
-    await app.close()
-  })
+    await app.close();
+  });
 
   beforeEach(() => {
-    execSync('npm run knex migrate:rollback --all')
-    execSync('npm run knex migrate:latest')
-  })
+    execSync("npm run knex migrate:rollback --all");
+    execSync("npm run knex migrate:latest");
+  });
 
-  test('user should be able to create a new transaction', async () => {
-    const response = await supertest(app.server).post('/transactions').send({
-      title: 'transaction test',
+  test("user should be able to create a new transaction", async () => {
+    const response = await supertest(app.server).post("/transactions").send({
+      title: "transaction test",
       amount: 5000,
-      type: 'credit',
-    })
+      type: "credit",
+    });
 
-    expect(response.status).toEqual(201)
-  })
+    expect(response.status).toEqual(201);
+  });
 
-  test('user should be able to get all transactions list', async () => {
+  test("user should be able to get all transactions list", async () => {
     const createTransaction = await supertest(app.server)
-      .post('/transactions')
+      .post("/transactions")
       .send({
-        title: 'transaction test',
+        title: "transaction test",
         amount: 5000,
-        type: 'credit',
-      })
-    const cookies = createTransaction.get('Set-Cookie')
+        type: "credit",
+      });
+    const cookies = createTransaction.get("Set-Cookie");
 
     const response = await supertest(app.server)
-      .get('/transactions')
-      .set('Cookie', cookies)
+      .get("/transactions")
+      .set("Cookie", cookies);
 
-    expect(response.status).toEqual(200)
+    expect(response.status).toEqual(200);
     expect(response.body.transactions).toEqual([
       expect.objectContaining({
-        title: 'transaction test',
+        title: "transaction test",
         amount: 5000,
       }),
-    ])
-  })
+    ]);
+  });
 
-  test('user should be able to get a specific transaction', async () => {
+  test("user should be able to get a specific transaction", async () => {
     const createTransaction = await supertest(app.server)
-      .post('/transactions')
+      .post("/transactions")
       .send({
-        title: 'transaction test',
+        title: "transaction test",
         amount: 5000,
-        type: 'credit',
-      })
-    const cookies = createTransaction.get('Set-Cookie')
+        type: "credit",
+      });
+    const cookies = createTransaction.get("Set-Cookie");
 
     const listTransactionsResponse = await supertest(app.server)
-      .get('/transactions')
-      .set('Cookie', cookies)
+      .get("/transactions")
+      .set("Cookie", cookies);
 
-    expect(listTransactionsResponse.status).toEqual(200)
+    expect(listTransactionsResponse.status).toEqual(200);
 
-    const transactionId = listTransactionsResponse.body.transactions[0].id
+    const transactionId = listTransactionsResponse.body.transactions[0].id;
 
     const getTransactionResponse = await supertest(app.server)
       .get(`/transactions/${transactionId}`)
-      .set('Cookie', cookies)
+      .set("Cookie", cookies);
 
-    expect(getTransactionResponse.status).toEqual(200)
+    expect(getTransactionResponse.status).toEqual(200);
     expect(getTransactionResponse.body.transaction).toEqual(
       expect.objectContaining({
-        title: 'transaction test',
+        title: "transaction test",
         amount: 5000,
-      }),
-    )
-  })
-
-  test('user should be able to get transactions summary', async () => {
-    const createTransaction = await supertest(app.server)
-      .post('/transactions')
-      .send({
-        title: 'transaction test',
-        amount: 5000,
-        type: 'credit',
       })
+    );
+  });
 
-    const cookies = createTransaction.get('Set-Cookie')
+  test("user should be able to get transactions summary", async () => {
+    const createTransaction = await supertest(app.server)
+      .post("/transactions")
+      .send({
+        title: "transaction test",
+        amount: 5000,
+        type: "credit",
+      });
+
+    const cookies = createTransaction.get("Set-Cookie");
 
     await supertest(app.server)
-      .post('/transactions')
-      .set('Cookie', cookies)
+      .post("/transactions")
+      .set("Cookie", cookies)
       .send({
-        title: 'transaction test',
+        title: "transaction test",
         amount: 2000,
-        type: 'debit',
-      })
+        type: "debit",
+      });
 
     const getSummaryResponse = await supertest(app.server)
-      .get('/transactions/summary')
-      .set('Cookie', cookies)
+      .get("/transactions/summary")
+      .set("Cookie", cookies);
 
-    expect(getSummaryResponse.status).toEqual(200)
+    expect(getSummaryResponse.status).toEqual(200);
     expect(getSummaryResponse.body.summary).toEqual(
       expect.objectContaining({
-        amount: 3000,
-      }),
-    )
-  })
-})
+        amount: 10,
+      })
+    );
+  });
+});
